9f90a5ccf91acdca7365c0068e4846c5a3e387a2
Karthik0-0 <pavankarthik107@gmail.com>
2024-06-25 22:56:50 +0530

    Js is Over and output is ready and need some changes

---
+++ script.js
@@ -0,0 +1,39 @@
+const list=document.getElementById("tasks");
+const inp=document.getElementById("in");
+const button=document.querySelector("button");
+
+button.addEventListener("click",()=>{
+    if (inp.value==" ")
+     alert("You must Write Something");
+    else{
+        let task=document.createElement("li");
+        let x=document.createElement("span");
+        x.textContent="x";
+        task.textContent=inp.value;
+        task.appendChild(x);
+        list.appendChild(task);
+    }
+    inp.value="";
+    save();
+})
+
+list.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        save();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        save();
+    }
+})
+
+function save(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function show(){
+    list.innerHTML=localStorage.getItem("data");
+}
+
+show();
